77a899e75140880ae076f11733cf359c3ccad08d
Csaba Moldovan <{{your_email}}>
2025-03-02 23:36:12 +0100

    Update changes on frontend

--- .next/static/chunks/pages/index.js
+++
@@ -1,8 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/node_modules_next_dist_2f8b308d._.js",
-  "static/chunks/node_modules_react-dom_82bb97c6._.js",
-  "static/chunks/node_modules_1b7400a8._.js",
-  "static/chunks/[root of the server]__3759f8a9._.js",
-  "static/chunks/pages_index_5771e187._.js",
-  "static/chunks/pages_index_7586f3ce._.js"
-])
--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    presets: [
-        '@babel/preset-env', // Transpile ES6+ syntax
-        ['@babel/preset-react', { runtime: 'automatic' }], // Enable JSX support
-    ],
-};
-
